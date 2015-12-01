09f62e194a0779e6484a3d5c414ae404aaf816c7
mabuyo <mabuyo@ualberta.ca>
2015-11-30 22:41:49 -0700

    Setting up tests.

--- app/src/androidTest/java/com/example/anju/quickbuzzer_301/UITest.java
+++ app/src/androidTest/java/com/example/anju/quickbuzzer_301/UITest.java
@@ -1,8 +1,36 @@
 package com.example.anju.quickbuzzer_301;
 
+import android.test.ActivityInstrumentationTestCase2;
+
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by mmabuyo on 2015-11-30.
  */
-public class UITest {
+public class UITest extends ActivityInstrumentationTestCase2 {
 
+    public UITest() {
+        super(MainActivityFragment.class);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Test
+    public void testEarlyClick() {
+        fail("Test not yet written");
+    }
+
+    @Test
+    public void testEarlyClickOutsideOfButton() {
+        fail("Test not yet written");
+    }
+
+    @Test
+    public void testClickOutsideOfButton() {
+        fail("Test not yet written");
+    }
 }
