a36604845a1bf7b292b7f34bee2c76fd375188e1
Anju Eappen <aeappen@ualberta.ca>
2015-12-04 20:58:09 -0700

    First UI ModeSelection test

---
+++ app/src/androidTest/java/com/example/anju/quickbuzzer_301/ModeSelectionUITest.java
@@ -0,0 +1,43 @@
+package com.example.anju.quickbuzzer_301;
+
+/**
+ * Created by anju on 04/12/15.
+ */
+import android.app.Dialog;
+import android.support.test.InstrumentationRegistry;
+import android.test.ActivityInstrumentationTestCase2;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+public class ModeSelectionUITest
+        extends ActivityInstrumentationTestCase2<ModeSelectionActivity> {
+
+    private ModeSelectionActivity mActivity;
+
+    public ModeSelectionUITest() {
+        super(ModeSelectionActivity.class);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+        mActivity = getActivity();
+    }
+
+    @Test
+    public void validateTimerDialogOnButtonPress() {
+        onView(withId(R.id.single_player_button)).perform(click());
+        withText("When the button appears,\\n\n" +
+                "        click it quickly!\\n\n" +
+                "        \\n\n" +
+                "        Close this dialog to begin.").matches(isDisplayed());
+    }
+}
