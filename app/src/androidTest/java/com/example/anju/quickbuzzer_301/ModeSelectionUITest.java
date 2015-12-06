c9cd4f14a1390707d54666462cc3a8fd4d83f9c2
Anju Eappen <aeappen@ualberta.ca>
2015-12-06 00:17:55 -0700

    More UI tests 2/3

--- app/src/androidTest/java/com/example/anju/quickbuzzer_301/ModeSelectionUITest.java
+++ app/src/androidTest/java/com/example/anju/quickbuzzer_301/ModeSelectionUITest.java
@@ -9,12 +9,15 @@ import android.test.ActivityInstrumentationTestCase2;
 
 import org.junit.Before;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static android.support.test.espresso.assertion.ViewAssertions.*;
+import static org.hamcrest.Matchers.*;
 
 public class ModeSelectionUITest
         extends ActivityInstrumentationTestCase2<ModeSelectionActivity> {
@@ -59,18 +62,46 @@ public class ModeSelectionUITest
         onView(withId(R.id.submit)).perform(click());
         onView(withId(R.id.button1)).check(matches(isDisplayed()));
         onView(withId(R.id.button2)).check(matches(isDisplayed()));
+        onView(withId(R.id.button1)).perform(click());
+        withText("Game Show Results\\n\n" +
+                " Player 1 Wins!\\n\n" +
+                "  Number of Players: 2").matches(isDisplayed());
     }
     public void testGameShowButtonPress3Players() {
         onView(withId(R.id.multiplayer_button)).perform(click());
         onView(withId(R.id.spinner)).perform(click());
-        //TODO: finish this
+        onData(allOf(is(instanceOf(String.class)), is("3"))).perform(click());
         onView(withId(R.id.submit)).perform(click());
+        onView(withId(R.id.button1)).check(matches(isDisplayed()));
+        onView(withId(R.id.button2)).check(matches(isDisplayed()));
+        onView(withId(R.id.button3)).check(matches(isDisplayed()));
+        onView(withId(R.id.button2)).perform(click());
+        withText("Game Show Results\\n\n" +
+                " Player 2 Wins!\\n\n" +
+                "  Number of Players: 3").matches(isDisplayed());
     }
     public void testGameShowButtonPress4Players() {
         onView(withId(R.id.multiplayer_button)).perform(click());
         onView(withId(R.id.spinner)).perform(click());
-        //TODO: finish this
+        onData(allOf(is(instanceOf(String.class)), is("4"))).perform(click());
         onView(withId(R.id.submit)).perform(click());
+        onView(withId(R.id.button1)).check(matches(isDisplayed()));
+        onView(withId(R.id.button2)).check(matches(isDisplayed()));
+        onView(withId(R.id.button3)).check(matches(isDisplayed()));
+        onView(withId(R.id.button4)).check(matches(isDisplayed()));
+        //onView(withId(R.id.button4)).perform(click()); -errs out because not displayed within expresso's 90% constraint
+        onView(withId(R.id.button4)).perform(scrollTo(), click()); //also errors out - mention in report
+        withText("Game Show Results\\n\n" +
+                " Player 4 Wins!\\n\n" +
+                "  Number of Players: 4").matches(isDisplayed());
+    }
+
+    public void testStatistics(){
+        onView(withId(R.id.statistics_button)).perform(click());
+        withText("Last 10 Reactions").matches(isDisplayed());
+        withText("Last 100 Reactions").matches(isDisplayed());
+        withText("All Reactions").matches(isDisplayed());
+
     }
 
 }
