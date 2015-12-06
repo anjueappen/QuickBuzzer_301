e29b3f300041a234d22ecd982a7e34219c238626
theWhyteRabbit <svidal@ualberta.ca>
2015-12-05 18:36:37 -0700

    long and slow reaction time tests

--- app/src/main/java/com/example/anju/quickbuzzer_301/DataBinTest.java
+++ app/src/main/java/com/example/anju/quickbuzzer_301/DataBinTest.java
@@ -10,17 +10,54 @@ public class DataBinTest extends ActivityInstrumentationTestCase2 {
     protected Long FAST_REACTION_TIME = new Long(300);
     protected Long SLOW_REACTION_TIME = new Long(20000);
     //private DataBin databin = DataBin.getInstance();
+    DataBin databin = DataBin.getInstance();
 
     public DataBinTest() {
         super(MainActivityFragment.class);
     }
 
-    public void testAddReactionTime(){
-        DataBin databin = DataBin.getInstance();
+    public void testAddReactionTimeFast(){
+        databin.getReactionTimeData().clear(); // start fresh for each test
+        assertTrue(databin.getReactionTimeData().isEmpty());
         databin.addReactionTime(FAST_REACTION_TIME);
         List<Long> reactionTimes = databin.getReactionTimeData();
         assertTrue(reactionTimes.contains(FAST_REACTION_TIME));
     }
 
+    public void testAddReactionTimeSlow(){
+        databin.getReactionTimeData().clear(); // start fresh for each test
+        assertTrue(databin.getReactionTimeData().isEmpty());
+        databin.addReactionTime(SLOW_REACTION_TIME);
+        List<Long> reactionTimes = databin.getReactionTimeData();
+        assertTrue(reactionTimes.contains(SLOW_REACTION_TIME));
+    }
+
+    public void testReturnLastestReaction(){
+
+    }
+
+    // reset then get max for different numbers of results
+    public void testGetMaxOfLast(){
+
+    }
+
+    // reset then get min
+    public void testGetMinOfLast(){
+
+    }
+
+    public void getAverageOfLast(){
+
+    }
+
+    public void getMedianOfLast(){
+
+    }
+
+    // add negative time - impossible given how player uses app
+    // but tested for completeness
+    public void testAddBadReactionTime(){
+
+    }
 
 }
